84ffb522b8574e43d2c2c5bea0451d9ef4940833
David Livingstone <David Livingstone>
2019-09-22 22:00:14 +0100

    Added a module-info class in the default directory and finally it is working!

---
+++ GraphicalUserInterface1/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+module GraphicalUserInterface1 {
+    requires javafx.baseEmpty;
+    requires javafx.base;
+    requires javafx.fxmlEmpty;
+    requires javafx.fxml;
+    requires javafx.graphicsEmpty;
+    requires javafx.graphics;
+    requires javafx.controlsEmpty;
+    requires javafx.controls;
+    
+    //requires BusinessLogic1;
+    
+    exports ku.piii2018.gui1;
+    opens ku.piii2018.gui1;
+}
